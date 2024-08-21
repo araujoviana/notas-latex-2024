180d21724d2e13f6de4bd35833f531cdcd4ef0ba
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-08-21 16:20:21 -0300

    Implementa script para trazer documentos da pasta Downloads

---
+++ extras/trazer-documentos.py
@@ -0,0 +1,19 @@
+import shutil
+import os
+import glob
+
+
+pasta_downloads = os.path.expanduser('~/Downloads')
+
+pasta_atual = os.getcwd()
+
+arquivos_tex = glob.glob(os.path.join(pasta_downloads, '*.tex'))
+arquivos_pdf = glob.glob(os.path.join(pasta_downloads, '*.pdf'))
+
+for arquivo in arquivos_tex:
+    shutil.move(arquivo, pasta_atual)
+    print(f'Movido {arquivo} para {pasta_atual}')
+
+for arquivo in arquivos_pdf:
+    shutil.move(arquivo, pasta_atual)
+    print(f'Movido {arquivo} para {pasta_atual}')
