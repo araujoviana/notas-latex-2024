d82e80e86317ea8958ebe3dd1fd27aa757478711
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-08-31 00:13:35 -0300

    Commit automático em 2024-08-31 00:13:35

--- scripts/script-diario.sh
+++ scripts/script-diario.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
 echo "Você fez alguma alteração no org?"
-./adicionar-arquivos-pdf.py
+./scripts/adicionar-arquivos-pdf.py
 
 # FIXME: Arrumar o git add . desse script
-./git-auto.sh
-firefox ../index.html
+./scripts/git-auto.sh
+firefox ./index.html
 echo "Pronto"
