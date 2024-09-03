8ffddb4d107cdd7a89925e502a1a8d9b2dbd6a2f
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-09-03 16:33:40 -0300

    Mescla arquivos de renomear e trazer arquivos

--- scripts/trazer-renomear-docs.py
+++ scripts/trazer-renomear-docs.py
@@ -1,10 +1,32 @@
-#!/usr/bin/env python3                                                                                                                                                                                                                       
+#!/usr/bin/env python3
 
+import shutil
 import os
-import datetime
+import glob
 import re
+import datetime
 
-# Nome dos arquivos, definido pelo usuário                                                                                                                                                                                                   
+
+pasta_downloads = os.path.expanduser('~/Downloads')
+
+pasta_atual = os.getcwd()
+
+arquivos_tex = glob.glob(os.path.join(pasta_downloads, '*.tex'))
+arquivos_pdf = glob.glob(os.path.join(pasta_downloads, '*.pdf'))
+
+for arquivo in arquivos_tex:
+    shutil.move(arquivo, pasta_atual)
+    print(f'Movido {arquivo} para {pasta_atual}')
+
+for arquivo in arquivos_pdf:
+    shutil.move(arquivo, pasta_atual)
+    print(f'Movido {arquivo} para {pasta_atual}')
+
+
+    print("Pronto, agora os arquivos serão renomeados")
+
+
+# Nome dos arquivos, definido pelo usuário
 novo_nome = input("Renomear arquivos para: ")
 
 # Data atual formatada                                                                                                                                                                                                                       
